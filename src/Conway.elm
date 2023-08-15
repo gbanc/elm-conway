05adf11a3a36be45824e37423308c0723a9b5f85
gabe ban <gabe.banciu@gmail.com>
2023-08-15 01:25:00 -0700

    added missing type hints

--- src/Conway.elm
+++ src/Conway.elm
@@ -21,13 +21,9 @@ init = List.map (\n -> 0) (range 1 (mapSize*mapSize))
 
 type Msg = NewGeneration | SetCellValue Int
 
-mapListByIndex : Int -> Int -> Int -> Int
-mapListByIndex cellpos pos val =
-    if pos == cellpos then
-        if val == 0 then 1
-        else 0
-    else val
 
+
+-- UPDATE 
 
 updateMatrix : Int -> List Int -> List Int
 updateMatrix cellpos matrix =
@@ -40,12 +36,24 @@ update msg model =
     SetCellValue cellpos ->
       updateMatrix cellpos model
 
+
+
+-- GENERATE VIEW
+
+mapListByIndex : Int -> Int -> Int -> Int
+mapListByIndex cellpos pos val =
+    if pos == cellpos then
+        if val == 0 then 1
+        else 0
+    else val
+
 generateRow = 
     let
         rangelist = range 1 mapSize
     in
     List.map (\n -> 0) rangelist
 
+getBackgroundColor : Int -> String
 getBackgroundColor celval =
     if celval == 1
     then "black"
@@ -73,16 +81,22 @@ toTableRow longlist counter =
     else 
         text ""
         
+view : List Int -> Html Msg
 view model =
   div []
     [ button [ onClick NewGeneration ] [ text "-" ]
     , table [] (List.map (toTableRow model) (range 0 (mapSize - 1)))
     ]
 
+
+
+-- CREATE LIFE
+
+life : List Int -> List Int
 life model =
     List.map (neighborCount model) (range 0 (mapSize*mapSize))
         
-
+neighborCount : List Int -> Int -> Int
 neighborCount matrix cellidx =
     let cellRow = cellidx//mapSize
         cellCol = modBy mapSize cellidx
@@ -101,11 +115,13 @@ neighborCount matrix cellidx =
         then 0
     else 0
 
+mapDeltaRows : Int -> Int -> List Int -> Int -> Int
 mapDeltaRows row column matrix deltaRow = 
     let delta = [-1, 0, 1]
     in
     List.foldl (+) 0 (List.map (mapDeltaCols deltaRow row column matrix) delta)
 
+mapDeltaCols : Int -> Int -> Int -> List Int -> Int -> Int
 mapDeltaCols delta_row row column model delta_col =
     let neighbor_row = modBy mapSize (row + delta_row)
         neighbor_col = modBy mapSize (column + delta_col)
@@ -116,7 +132,7 @@ mapDeltaCols delta_row row column model delta_col =
         0
     else
         Maybe.withDefault 0 (get idx model)
-        
+get : Int -> List Int -> Maybe Int       
 get n xs  = List.head (List.drop n xs)
 
 
